d1fb532a15a3b2c99a14892fa641700f716bf2f5
Brendan Nash <brendan@southeasterdesign.com>
2019-04-02 14:36:12 +0200

    Moved the generic pack of cards to its own method.

--- src/Command/Rng.php
+++ src/Command/Rng.php
@@ -75,7 +75,21 @@ class Rng
     }
 
     /**
-     * Simple heads or tails game
+     * Generic pack of cards
+     * @return array
+     */
+    private function getCardPack()
+    {
+        return [
+            0 => '2C', 1 => '3C', 2 => '4C', 3 => '5C', 4 => '6C', 5 => '7C', 6 => '8C', 7 => '9C', 8 => '10C', 9 => 'JC', 10 => 'QC', 11 => 'KC', 12 => 'AC',
+            13 => '2D', 14 => '3D', 15 => '4D', 16 => '5D', 17 => '6D', 18 => '7D', 19 => '8D', 20 => '9D', 21 => '10D', 22 => 'JD', 23 => 'QD', 24 => 'KD', 25 => 'AD',
+            26 => '2H', 27 => '3H', 28 => '4H', 29 => '5H', 30 => '6H', 31 => '7H', 32 => '8H', 33 => '9H', 34 => '10H', 35 => 'JH', 36 => 'QH', 37 => 'KH', 38 => 'AH',
+            39 => '2S', 40 => '3S', 41 => '4S', 42 => '5S', 43 => '6S', 44 => '7S', 45 => '8S', 46 => '9S', 47 => '10S', 48 => 'JS', 49 => 'QS', 50 => 'KS', 51 => 'AS',
+        ];
+    }
+
+    /**
+     * Simple heads or tails game using the getBoolean method
      * @param AdapterInterface $console
      */
     private function headsOrTails(AdapterInterface $console)
@@ -125,18 +139,13 @@ class Rng
     {
         $loop = true;
 
+        $cardPack = $this->getCardPack();
+
         /**
          * Some trivia, cards are valued in the following order:
          * Clubs, Diamonds, Hearts, Spades, with the two of Clubs being the weakest card and the Ace of Spades being the strongest.
          * Why? Because it is alphabetic!
          */
-        $cardPack = [
-            0 => '2C', 1 => '3C', 2 => '4C', 3 => '5C', 4 => '6C', 5 => '7C', 6 => '8C', 7 => '9C', 8 => '10C', 9 => 'JC', 10 => 'QC', 11 => 'KC', 12 => 'AC',
-            13 => '2D', 14 => '3D', 15 => '4D', 16 => '5D', 17 => '6D', 18 => '7D', 19 => '8D', 20 => '9D', 21 => '10D', 22 => 'JD', 23 => 'QD', 24 => 'KD', 25 => 'AD',
-            26 => '2H', 27 => '3H', 28 => '4H', 29 => '5H', 30 => '6H', 31 => '7H', 32 => '8H', 33 => '9H', 34 => '10H', 35 => 'JH', 36 => 'QH', 37 => 'KH', 38 => 'AH',
-            39 => '2S', 40 => '3S', 41 => '4S', 42 => '5S', 43 => '6S', 44 => '7S', 45 => '8S', 46 => '9S', 47 => '10S', 48 => 'JS', 49 => 'QS', 50 => 'KS', 51 => 'AS',
-
-        ];
 
         while ($loop == true) {
             $figlet = new \Zend\Text\Figlet\Figlet();
